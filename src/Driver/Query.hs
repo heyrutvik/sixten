6fdbaee2a47c0e087251d38d65d05e8d67c501be
Olle Fredriksson <fredriksson.olle@gmail.com>
2018-11-10 20:00:02 +0100

    wip

--- src/Driver/Query.hs
+++ src/Driver/Query.hs
@@ -1,20 +1,28 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
 module Driver.Query where
 
 import Protolude
 
+import Data.Dependent.Map(DMap)
+import qualified Data.Dependent.Map as DMap
 import Data.HashMap.Lazy(HashMap)
+import qualified Data.HashMap.Lazy as HashMap
 import Rock
 import Text.Parsix.Position
 
+import Error
+import qualified Frontend.Parse as Parse
+import Processor.Result
 import Syntax
 import qualified Syntax.Core as Core
 import qualified Syntax.Pre.Definition as Pre
 import qualified Syntax.Pre.Scoped as Pre
 import qualified Syntax.Pre.Unscoped as Unscoped
 
-type ParsedModule = HashMap QName (SourceLoc, Unscoped.TopLevelDefinition)
+type ModuleDefinitions = HashMap QName (SourceLoc, Unscoped.TopLevelDefinition)
 type ResolvedModule = HashMap QName [(QName, SourceLoc, Closed (Pre.Definition Pre.Expr))]
 type TypeCheckedGroup = HashMap QName (SourceLoc, ClosedDefinition Core.Expr, Biclosed Core.Expr)
 
@@ -22,32 +30,38 @@ data Query a where
   -- Frontend
   Files :: Query [FilePath]
   File :: FilePath -> Query Text
-  ModuleHeader :: FilePath -> Query (ModuleHeader, Position)
+  ParsedModule :: FilePath -> Query (ModuleHeader, [(SourceLoc, Unscoped.TopLevelDefinition)])
   ModuleHeaders :: Query (HashMap FilePath ModuleHeader)
-  ParsedModule :: ModuleName -> Query ParsedModule
   ResolvedModule :: ModuleName -> Query ResolvedModule
   TypeCheckedGroup :: QName -> Query TypeCheckedGroup
 
   Type :: QName -> Query (Biclosed Core.Expr)
   Definition :: QName -> Query (ClosedDefinition Core.Expr)
 
-rules :: [FilePath] -> Rules Query
-rules inputFiles query = case query of
-  Files -> Input $ return inputFiles
-  File file -> Input $ readFile file
-  Driver.Query.ModuleHeader file -> undefined
-  ModuleHeaders -> do
+noError :: (Monoid w, Functor f) => f a -> f (a, w)
+noError = fmap (, mempty)
+
+rules :: [FilePath] -> GenRules (Writer [Error] Query) Query
+rules inputFiles (Writer query) = case query of
+  Files -> Input $ noError $ return inputFiles
+  File file -> Input $ noError $ readFile file
+  ParsedModule file -> Task $ do
+    text <- fetch $ File file
+    case Parse.parseText Parse.modul text file of
+      Failure errs -> do
+        let mh = ModuleHeader "Main" mempty mempty
+        return ((mh, mempty), errs)
+      Success a -> return (a, mempty)
+  ModuleHeaders -> Task $ noError $ do
     fileNames <- fetch Files
-    forM fileNames $ \file ->
-      fetch $ File file
-
-  ParsedModule -> undefined
-  ResolvedModule -> undefined
-  TypeCheckedGroup -> undefined
-
-  Type -> undefined
-  Definition -> undefined
+    result <- for fileNames $ \file ->
+      (,) file <$> fetchModuleHeader file
+    return $ HashMap.fromList result
+  ResolvedModule moduleName -> undefined
+  TypeCheckedGroup name -> undefined
+  Type name -> undefined
+  Definition name -> undefined
 
 -- Derived queries
 fetchModuleHeader :: FilePath -> Task Query ModuleHeader
-fetchModuleHeader = fst <$> fetch ParseModuleHeader
+fetchModuleHeader file = fst <$> fetch (ParsedModule file)
